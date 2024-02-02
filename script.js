91ed87a9210c31d17b4787c1b618b85c3dc3b1bb
Polar <readingworld2@gmail.com>
2024-02-02 14:29:32 -0500

    Below the team property, add a new property with a key named sport and a string value of Football.

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
-const myFavoriteFootballTeam = { team: "Argentina" };
+const myFavoriteFootballTeam = { team: "Argentina", sport: "Football" };

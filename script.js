cd15b6620f1af29a2abcde177bb04d25d15bbfc1
Polar <readingworld2@gmail.com>
2024-02-05 18:05:35 -0500

    Inside the template literals, add an empty div with a class of player-card.

--- script.js
+++ script.js
@@ -184,6 +184,8 @@ headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
   playerCards.innerHTML += arr.map(
-    ({ name, position, number, isCaptain, nickname }) => {}
+    ({ name, position, number, isCaptain, nickname }) => {
+      `<div class="player-card"></div>`;
+    }
   );
 };

636a559820af69b8ce5f808ecf188a764100e384
Polar <readingworld2@gmail.com>
2024-02-05 18:16:21 -0500

    To remove the commas between each player-card so it does not show up on screen, chain the .join() method to the .map() method. Pass an empty string as the argument for the .join() method

--- script.js
+++ script.js
@@ -183,14 +183,14 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr.map(
-    ({ name, position, number, isCaptain, nickname }) => {
+  playerCards.innerHTML += arr
+    .map(({ name, position, number, isCaptain, nickname }) => {
       `<div class="player-card">
       <h2>${name}${isCaptain ? "(Captain)" : ""}</h2>
       <p>Position: ${position}</p>
       <p>Number: ${number} </p>
       <p>Nickname: ${nickname ? nickname : "N/A"} </p>
       </div>`;
-    }
-  );
+    })
+    .join("");
 };

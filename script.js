d4df62c2435cdd7955ee0ff953006f569ebf6d74
Polar <readingworld2@gmail.com>
2024-02-05 13:25:00 -0500

    Assign the year variable to worldCupYear.textContent and assign the coachName variable to headCoach.textContent

--- script.js
+++ script.js
@@ -179,3 +179,5 @@ const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
 typeOfSport.textContent = sport;
 teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
